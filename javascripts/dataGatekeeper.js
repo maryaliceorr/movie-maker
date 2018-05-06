0f8ed1d1e8d546a6c50daba997604803d9fc0f25
Mary Alice Orr <m.a.orr17@gmail.com>
2018-05-06 16:04:20 -0500

    got elements printed in categories and styled them

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -1,20 +1,18 @@
 const data = require('./data');
 const categories = require('./categories');
 const movieElements = require('./movieElements');
-const elementsDom = require('./elementsDom');
 const mainDom = require ('./mainDom');
 
 const whenCategoriesLoad = function () {
   const categoriesData = JSON.parse(this.responseText).categories;
   data.setCategories(categoriesData);
-  mainDom.printToDomMain(categoriesData);
+  movieElements.loadMovieElements(whenElementsLoad, nope);
 };
 
 const whenElementsLoad = function () {
   const elementsData = JSON.parse(this.responseText).movieElements;
   data.setMovieElements(elementsData);
-  console.log('elementsData', elementsData);
-  elementsDom.printToDomElements(elementsData);
+  mainDom.printToDomMain();
 };
 
 const nope = function () {
@@ -23,7 +21,6 @@ const nope = function () {
 
 const initializer = () => {
   categories.loadCategories(whenCategoriesLoad, nope);
-  movieElements.loadMovieElements(whenElementsLoad, nope);
 };
 
 module.exports = {
--- javascripts/elementsDom.js
+++ javascripts/elementsDom.js
@@ -1,21 +1,20 @@
-const elementsOutputDiv = document.getElementById('elements-holder');
+// const elementsOutputDiv = document.getElementById('elements-holder');
 
-const elementList = (elements) => {
-  let domStrang = '';
-  elements.forEach((element) => {
-    domStrang += `<div class="elements form-check">`;
-    domStrang += `<input class="form-check-input" type="checkbox" id="defaultCheck1">`;
-    domStrang += `<label class="form-check-label" for="defaultCheck1">${element.name}</label>`;
-    domStrang += `</div>`;
-    console.log(domStrang);
-  });
-  return domStrang;
-};
+// const elementList = (category) => {
+//   let domStrang = '';
+//   elements.forEach((element) => {
+//     domStrang += `<div class="elements form-check">`;
+//     domStrang += `<input class="form-check-input" type="checkbox" id="defaultCheck1">`;
+//     domStrang += `<label class="form-check-label" for="defaultCheck1">${element.name}</label>`;
+//     domStrang += `</div>`;
+//   });
+//   return domStrang;
+// };
 
-const printToDomElements = (elements) => {
-  elementsOutputDiv.innerHTML = elementList(elements);
-};
+// const printToDomElements = (elements) => {
+//   elementsOutputDiv.innerHTML = elementList(elements);
+// };
 
-module.exports = {
-  printToDomElements,
-};
+// module.exports = {
+//   printToDomElements,
+// };
